26561edbae04d81285d7a60a8625fdfd92289e28
O-Alexsandro <alexsandrooliveiraribas@gmail.com>
2025-04-05 08:17:11 -0300

    Iniciando o projeto

---
+++ Pata-segura/src/main/java/br/com/pata/segura/PataSeguraApplication.java
@@ -0,0 +1,13 @@
+package br.com.pata.segura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PataSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PataSeguraApplication.class, args);
+	}
+
+}
---
+++ Pata-segura/src/test/java/br/com/pata/segura/PataSeguraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pata.segura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PataSeguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
